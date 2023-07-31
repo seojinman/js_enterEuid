9c5a706968ba83e452bcfcfdccc01cdfcac4012d
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-07-31 13:26:50 +0900

    Feat: EUID App UI main 메인(홈) 페이지 JS 초안
    메인(홈) 페이지의 Carousel 부분에 Swiper.js를 활용하여, 슬라이드 기능을 만들었습니다.
    
    이전, 다음 버튼 클릭시 해당 슬라이드를 넣고, 자동으로 흘러가는 autoplay, 그리고 pagination을 일부 커스텀하였습니다.
    
    추가적으로, 각 이듬 홈페이지의 일부분을 발췌하여 dataset을 만들었고, 이를 서버에서 렌더링하여 클라이언트에서 받아오게 했습니다. 마지막으로, 각 자세히 보기 버튼을 누를 시, 실제 이듬 홈페이지로 연결했습니다.
    
    fixes: 추후 요구 사항반영 및 기능 추가로 구조 변경 가능성 있음
    Related to #7, #35

---
+++ client/js/main.js
@@ -0,0 +1,49 @@
+const swiperWrapper = document.getElementById('swiper-wrapper');
+
+fetch('http://localhost:3000/euid')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(slide => {
+      const slideDiv = `
+        <div class="swiper-slide relative w-[20rem] h-[15.9375rem] bg-[#9DA1B4]">
+          <h3 class="sr-only">${slide.name}</h3>
+          <p class="text-white font-medium text-[1rem] absolute overflow-elliasis whitespace-normal line-clamp-2 max-w-[200px] max-h-[60px] leading-normal left-[30px] top-[30px]">
+            ${slide.description}
+          </p>
+          <img class="absolute w-[13.75rem] right-0 bottom-0" src="${slide.image.thumbnail}" alt="${slide.image.alt}" />
+          <a href="${slide.link}" target="_blank" class="text-black absolute text-[0.6875rem] left-[30px] top-[85px]">자세히 보기</a>
+        </div>
+      `;
+      swiperWrapper.insertAdjacentHTML('beforeend', slideDiv);
+    });
+
+    // Initialize Swiper after creating slides
+    const mainSwiper = new Swiper('.swiper .swiper-container', {
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      speed: 1500,
+      slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      preventClicks: false,
+      preventClicksPropagation: false,
+    });
+
+    const pageSwiper = new Swiper('.swiper .swiper-container', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination2',
+        type: 'progressbar',
+      },
+    });
+    mainSwiper.controller.control = pageSwiper;
+  });
